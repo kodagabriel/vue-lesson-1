e3ccc60c9f56c9903b6a177ea99ee27728569ba4
Koda Gabriel <kodagabriel@outlook.com>
2023-09-09 21:09:06 -0300

    computed

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ const app = Vue.createApp({
         toggleFav(book) {
             book.isFav = !book.isFav
         }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter( (book) => { book.isFav } )
+        }
     }
 })
 
